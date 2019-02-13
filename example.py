877ba0997d2abf6caa3404d1771072c87e3572f8
Michael Kapteyn <mkapteyn@mit.edu>
2019-02-13 16:05:47 -0500

    Add shmm class, fwd_bkw alg

--- example.py
+++ example.py
@@ -9,15 +9,41 @@ from shmm.utils import *
 
 DAMAGE_LIB_PATH = './damageLibrary.json'
 
-# create damage library object
-d = damageLibrary(DAMAGE_LIB_PATH)
-
-# generate artificial measurements - 100 measurements using linear interpolation through all states
-nMeasurements = 200
-m = measurementGenerator(d)
-noise = noiseParams(d.nSensors,"Gaussian")
-meas = m.genMeasurements(nMeasurements, d.states, type='linear')
-
-print(meas)
+# # create damage library object
+# d = damageLibrary(DAMAGE_LIB_PATH)
+#
+# # generate artificial measurements - 100 measurements using linear interpolation through all states
+# nMeasurements = 200
+# m = measurementGenerator(d)
+# noise = noiseParams(d.nSensors,"Gaussian")
+# meas = m.genMeasurements(nMeasurements, d.states, type='linear')
+#
+# print(meas)
 # Perform smoothing
 #TODO
+
+dl = read_json_file('./damageLibrary.json')
+
+states = list(dl.keys())[:-1]
+end_state = states[-1]
+
+observations = np.array(([0,0,0,1,1,1,2,2,2,3,3,3,4,4,4]))
+
+start_probability = np.zeros(len(states))
+start_probability[0] = 1
+
+tp = np.array(([0.9, 0.1, 0, 0, 0]))
+transition_probability = tp
+for i in range(1,len(states)):
+   transition_probability = np.vstack((transition_probability,np.roll(tp,i)))
+
+
+
+ep = np.array(([0.5, 0.25, 0, 0, 0.25]))
+emission_probability = ep
+for i in range(1,len(states)):
+   emission_probability = np.vstack((emission_probability,np.roll(ep,i)))
+
+
+fwd,bkw,pos = fwd_bkw(observations, states, start_probability, transition_probability, emission_probability, end_state)
+print(pos)
--- shmm/shmm.py
+++ shmm/shmm.py
@@ -3,19 +3,92 @@ import numpy as np
 from shmm.damageLibrary import *
 from shmm.measurementGenerator import *
 from shmm.utils import *
-
+from queue import *
+from functools import reduce
 
 class SHMM:
-    def __init__(self, damageLibrary, measurements, transitionProbabilities):
-        self.initialprob = np.zeros(len(damage.library.states))
-        self.initialprob[0] = 1
-        self.transitionmat = self.computeTransitionMatrix()
-        self.emissionmat = self.computeEmissionMatrix()
+    def __init__(self, damageLibrary, measurements, initialProb=None, transitionMat=None, emissionMat=None):
+        self.damageLibrary = damageLibrary
+        self.measurements = measurements
+
+        if initialProb is None:
+            self.initialprob = np.zeros(len(damage.library.states))
+            self.initialprob[0] = 1
+        else:
+            self.initialProb = np.array(initialProb)
+
+
+        if transitionMat is None:
+            self.TM = self.computeTransitionMatrix()
+        else:
+            self.TM = transitionMat
+
+        if emissionMat is None:
+            self.OM = self.computeEmissionMatrix()
+        else:
+            self.OM = emissionMat
+
+        self.delay = 20
+
+        self.events = Queue()
+        self.t = 0
+        self.B = np.ndarray(self.TM.shape)
+        self.forward = np.dot(self.initialProb, self.TM)
+        self.backward = np.dot(self.initialProb, self.TM)
 
     def computeTransitionMatrix(self):
-        #TODO
-        return transitionMat
+        T = []
+        return T
 
     def computeEmissionMatrix(self):
-        #TODO
-        return emissionMat
+        O =[]
+        return O
+
+def fwd_bkw(observations, states, start_prob, trans_prob, emm_prob, end_st):
+    end_st = 4
+    # forward part of the algorithm
+    fwd = []
+    f_prev = {}
+    for i, observation_i in enumerate(observations):
+        f_curr = np.zeros((len(states)))
+        for st,p in enumerate(states):
+            if i == 0:
+                # base case for the forward part
+                prev_f_sum = start_prob[st]
+            else:
+                prev_f_sum = sum(f_prev[k]*trans_prob[k][st] for k,s in enumerate(states))
+
+            f_curr[st] = emm_prob[st][observation_i] * prev_f_sum
+
+        fwd.append(f_curr)
+        f_prev = f_curr
+
+    p_fwd = sum(f_curr[k] * trans_prob[k][end_st] for k,s in enumerate(states))
+
+    # backward part of the algorithm
+    bkw = []
+    b_prev = np.zeros((len(states)))
+    for i, observation_i_plus in enumerate(reversed(np.append(observations[1:],0))):
+        b_curr = np.zeros((len(states)))
+        for st,s in enumerate(states):
+            if i == 0:
+                # base case for backward part
+                b_curr[st] = trans_prob[st][end_st]
+            else:
+                b_curr[st] = sum(trans_prob[st][l] * emm_prob[l][observation_i_plus] * b_prev[l] for l,s in enumerate(states))
+
+        bkw.insert(0,b_curr)
+        b_prev = b_curr
+
+    p_bkw = sum(start_prob[l] * emm_prob[l][observations[0]] * b_curr[l] for l,s in enumerate(states))
+
+    # merging the two parts
+    posterior = np.zeros((len(observations),len(states)))
+    # print(len(bkw))
+    # print(len(fwd))
+    for i in range(len(observations)):
+        for st,s in enumerate(states):
+            posterior[i,st] = fwd[i][st] * bkw[i][st] / p_fwd
+        # posterior.append({st: fwd[i][st] * bkw[i][st] / p_fwd for st,s in enumerate(states)})
+    assert p_fwd-p_bkw < 1e-5
+    return fwd, bkw, posterior
